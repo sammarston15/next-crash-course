661a6d35c57eace29c0296c8c9c730608793c429
Sam Marston <smarston@easypost.com>
2022-10-24 03:57:20 -0700

    finished crash course

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+// DEFAULT LAYOUT FOR THIS FILE
+// import '../styles/globals.css'
+
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
+
+// export default MyApp
+
+import Layout from "../components/Layout";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
-export default MyApp
+export default MyApp;
